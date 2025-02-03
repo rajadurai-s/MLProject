c242f5da91aa2a16f65483e8fcc7730db8ff63d1
SRajadurai <mrraja212121@gmail.com>
2025-02-03 16:18:55 +0530

    Logging and Exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from src.logger import logging #to commit exceptions into logging
 
 #own custom message
 def error_message_detail(error,error_detail:sys):
