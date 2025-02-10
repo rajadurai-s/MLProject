3876b364635a9bb1c5e1fce3057529c7b3da3693
SRajadurai <mrraja212121@gmail.com>
2025-02-11 00:23:41 +0530

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,7 +47,50 @@ class ModelTrainer:
                 "AdaBoost Regressor":AdaBoostRegressor()
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            '''
+            Hyper parameter tuning
+            '''
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    #'splitter':['best','random'],
+                    #'max_features':['sqrt','log2']
+                },
+                "Random Forest":{
+                    #'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    #'max_features':['sqrt','log2',None],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                'Gradient Boosting':{
+                    #'loss':['sqaured_error','huber','absolute_error','quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    #'criterion':['squared_error','friedman_mse'],
+                    #'max_features':['sqrt','log2','auto'],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                'Linear Regressor':{},
+                "K-Neighbor Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    #'weights':['uniform','distance'],
+                    #'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "XGB Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Cat Boosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,params=params)
 
             ##To get best model score
             best_model_score=max(sorted(model_report.values()))
